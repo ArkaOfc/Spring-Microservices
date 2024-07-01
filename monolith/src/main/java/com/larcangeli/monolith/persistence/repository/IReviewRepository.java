8526b5a8e4ddca3a2cf72ca6334bf3d47fb7d5e6
ArkaOfc <102794103+ArkaOfc@users.noreply.github.com>
2024-07-01 16:58:31 +0200

    Delete monolith directory

--- monolith/src/main/java/com/larcangeli/monolith/persistence/repository/IReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.larcangeli.monolith.persistence.repository;
-
-import com.larcangeli.monolith.persistence.model.Review;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IReviewRepository extends CrudRepository<Review, Long> {
-}
